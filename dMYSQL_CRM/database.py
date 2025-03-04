4a5d516febd65af3fc0226b8c0c04075711b19a8
Md. Faysal Ahmed <falampur@gmail.com>
2025-03-04 16:54:31 -0330

    created the database

--- dMYSQL_CRM/database.py
+++ dMYSQL_CRM/database.py
@@ -1,1 +1,15 @@
-import mysql.conncetor
+import mysql.connector
+
+data_base = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    passwd = 'password321',
+    )
+
+#prepare the cursor object
+cursor_object = data_base.cursor()
+
+#create a database
+cursor_object.execute("CREATE DATABASE garments")
+
+print("hey, it's done !")
